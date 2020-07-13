67a5ced0e65003bd1168c60e5136ad9d79116b12
aman rathore <aman@kali.aman>
2020-07-13 03:24:33 -0400

    Done

---
+++ www/js/Login.js
@@ -0,0 +1,22 @@
+let onclick = document.getElementById('b1');
+onclick.addEventListener('click' , function(){
+	location.reload(true);
+	const name = document.getElementById('text').value;
+	const pass = document.getElementById('password').value;
+	if(name === "Aman" && pass === "123")
+	{
+		window.open("js/error.html");
+	}
+	else if(name === "" || pass=== "")
+	{
+		document.open("error.html");
+		alert("Field should not be empty");
+	}
+	else
+	{
+		document.open("error.html");
+		alert("Try again");
+	}
+});
+
+
